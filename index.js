5f9811c582a6ea301a6568b9844968a1a56dcf78
pajosego <144797657+pajosego@users.noreply.github.com>
2025-07-21 09:57:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-
+require('dotenv').config(); // Isto pode continuar, não interfere
 const { monitor } = require('./monitor');
 
 const CHAT_ID = process.env.CHAT_ID;
 
-if (!CHAT_ID) {
-  console.error('❌ Variável CHAT_ID não definida.');
-  process.exit(1);
-}
-
 async function start() {
+  if (!CHAT_ID) {
+    console.error('❌ Variável CHAT_ID não definida.');
+    return;
+  }
+
   await monitor(CHAT_ID);
   setInterval(() => monitor(CHAT_ID), 15 * 60 * 1000);
-  process.stdin.resume(); // Railway
+
+  process.stdin.resume();
 }
 
 start();
